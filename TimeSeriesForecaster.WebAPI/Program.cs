77d2a370de08b17a7781967a97d48f5f159070b0
gulniha <gvurat@hotmail.com>
2025-10-19 16:52:25 +0300

    appsettings.Development.json takibi bırakıldı.

--- TimeSeriesForecaster.WebAPI/Program.cs
+++ TimeSeriesForecaster.WebAPI/Program.cs
@@ -14,7 +14,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: reactAppPolicy,
                       policy =>
                       {
-                          policy.WithOrigins("http://localhost:5174")
+                          policy.WithOrigins("http://localhost:5173")
                                 .AllowAnyHeader()
                                 .AllowAnyMethod();
                       });
