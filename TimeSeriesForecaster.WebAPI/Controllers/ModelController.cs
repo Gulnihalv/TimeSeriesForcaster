73629be2e4270dd8e99254f43671f61b4cd34ed0
Gulnihalv <gulnihal.vurat@st.uskudar.edu.tr>
2026-07-03 20:33:50 +0300

    ara öncesi son düzenlemeler

---
+++ TimeSeriesForecaster.WebAPI/Controllers/ModelController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TimeSeriesForecaster.WebAPI.Controllers;
+
+[Authorize]
+[ApiController]
+[Route("api/datasets")]
+public class ModelController
+{
+    
+}
